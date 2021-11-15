03223475ed71216f822d964cab6aa3291c8fd79c
Tai Pham Phu <phamphutaitrainer@gmail.com>
2021-11-15 21:08:48 +0700

    Authentication service

---
+++ src/services/authentication/authentication.service.ts
@@ -0,0 +1,5 @@
+import { ILoginState, IRegisterNewAccountState } from "models/IUserState";
+
+export const login = async (loginState: ILoginState) => {};
+
+export const register = async (registerState: IRegisterNewAccountState) => {};
