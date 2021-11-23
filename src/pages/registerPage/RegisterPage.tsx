f17eda131262fb263bb7748b9ed7b1cadf992611
Phat Do <dodangphat99@gmail.com>
2021-11-23 18:05:33 +0700

    update

--- src/pages/registerPage/RegisterPage.tsx
+++ src/pages/registerPage/RegisterPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as Style from "pages/registerPage/style";
 import { Image, Space } from "antd";
+import {Link} from "react-router-dom";
 import imageRegister from "assets/images/register.png";
 import { UserOutlined, MailOutlined, PhoneOutlined } from "@ant-design/icons";
 
@@ -8,120 +9,18 @@ const RegisterPage = (props: any) => {
   const { history } = props;
 
   return (
-    <div className="ant">
-      <Style.Leftpage>
-        <Style.Title>TASKEEPER</Style.Title>
-        <Image
-          style={{ marginTop: "300px" }}
-          width={500}
-          height={400}
-          src={imageRegister}
-        />
-      </Style.Leftpage>
-
-      <Style.RegisterForm
-        name="normal_register"
-        initialValues={{
-          remember: true,
-        }}
-      >
-        <Style.Signup>Sign up to Taskeeper</Style.Signup>
-        <Style.Rightpage>
-          <Style.TitleView
-            name="firstname"
-            rules={[
-              {
-                required: true,
-                message: "Please input your first name!",
-              },
-            ]}
-          >
-            <Style.Firstname
-              placeholder="First Name"
-              prefix={
-                <UserOutlined
-                  style={{ marginLeft: "10px", marginRight: "10px" }}
-                />
-              }
-              size="large"
-            />
-          </Style.TitleView>
-          <Style.TitleView
-            name="lastname"
-            rules={[
-              {
-                required: true,
-                message: "Please input your last name!",
-              },
-            ]}
-          >
-            <Style.Lastname
-              placeholder="Last Name"
-              prefix={
-                <UserOutlined
-                  style={{ marginLeft: "10px", marginRight: "10px" }}
-                />
-              }
-              size="large"
-            />
-          </Style.TitleView>
-          <Style.TitleView
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Email or Phone!",
-              },
-            ]}
-          >
-            <Style.Email
-              placeholder="Email"
-              prefix={
-                <MailOutlined
-                  style={{ marginLeft: "10px", marginRight: "10px" }}
-                />
-              }
-              size="large"
-            />
-          </Style.TitleView>
-          <Space direction="vertical">
-            <Style.TitleView
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your password!",
-                },
-              ]}
-            >
-              <Style.Password placeholder="Password" />
-            </Style.TitleView>
-          </Space>
-          <Style.TitleView
-            name="phonenumber"
-            rules={[
-              {
-                required: true,
-                message: "Please input your phone number!",
-              },
-            ]}
-          >
-            <Style.PhoneNumber
-              placeholder="Phone Number"
-              prefix={
-                <PhoneOutlined
-                  style={{ marginLeft: "10px", marginRight: "10px" }}
-                />
-              }
-              size="large"
-            />
-          </Style.TitleView>
-        </Style.Rightpage>
-
-        <Style.RegisterFormButton type="primary" htmlType="submit">
-          <Style.RegisterText>REGISTER</Style.RegisterText>
-        </Style.RegisterFormButton>
-      </Style.RegisterForm>
+    <div className="ant" style={{flexDirection:'row',display:'flex',width:"100vw",height:"100vh"}}>
+      <div style={{width:'40%',height:'100%',backgroundColor:'#add2c9'}}>
+        <Image style={{width:'100%',height:'100%'}} src={imageRegister}/>
+      </div>
+      <div style={{width:'60%',height:'100%'}}>
+        <div style={{flexDirection:'column',textAlign:'end',margin:20}}>
+          <p>Already have an account? <Link style={{color:'red',fontWeight:'bold'}} to="/">Sign in</Link></p>   
+        </div>
+        <p style={{margin:50,fontWeight:'bold',fontSize:35,textAlign:'center'}}>Sign up to Taskeeper</p>
+        <div style={{width:'100%',height:'65%'}}>
+        </div>
+      </div>
     </div>
   );
 };
